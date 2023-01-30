8dd80bfcbdacb4196fd7f7142c30bbefa9c600a8
Kent vejrup Madsen <kent.vejrup.madsen@designermadsen.dk>
2023-01-30 18:10:46 +0100

    structuring data

--- index.php
+++ index.php
@@ -6,36 +6,4 @@
 	
 	use \IOJaegers\Valkyrier\Extractor
 		as Extractor;
-	
-	
-	
-	$find = array(l1, l2, l3, l4, l5, l6, l7);
-	
-	echo "====== TEST SCRIPT FOR DEVELOPMENT ========================================\r\n";
-	const class_name = '"image"';
-
-	$index = 0;
-	$sizeOf = count($find);
-	
-	for(
-		$index = 0;
-		$index < $sizeOf;
-		$index ++
-	)
-	{
-		$current = $find[$index];
-		
-		$extractor1 = new Extractor(
-			new RD(
-				$current
-			),
-			"https://commons.wikimedia.org"
-		);
-		
-		$extracted_1 = $extractor1->retrieveUrlsByClassName( class_name );
-		
-		print_r($extracted_1);
-	}
-	
-	
 ?>
